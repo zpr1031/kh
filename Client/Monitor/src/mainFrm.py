8d2eb0709f66cb48ecf3a527a1edab555e62e177
yyh <zpr1031@gmail.com>
2018-01-03 11:27:36 +0900

    first Init

---
+++ Client/Monitor/src/mainFrm.py
@@ -0,0 +1,19 @@
+'''
+Created on 2018. 1. 3.
+
+@author: user1
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+
+if __name__  == "__main__":
+    pass
